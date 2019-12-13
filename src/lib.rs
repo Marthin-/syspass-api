032f9bc029338dabfab8503bb20b31a5335fef50
Martin GUILLOUX <martin.guilloux@cognix-systems.com>
2019-12-13 19:32:16 +0100

    better json deserialization

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 #![allow(unused_must_use)]
 extern crate json;
 extern crate reqwest;
@@ -15,13 +16,29 @@ pub struct JsonReq {
     jsonrpc: String,
     method: String,
     params: HashMap<String, String>,
-    id: i8,
+    id: u8,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct APIResult {
+    count: u8,
+    itemId: u16,
+    result: Vec<Value>,
+}
+
+#[derive(Deserialize, Debug, Serialize)]
+pub struct APIOutput {
+    id: u8,
+    jsonrpc: String,
+    result: APIResult,
+    resultcode: u16,
+    resultMessage: Option<String>,
 }
 
 /*
 * Send a json request to given url 
 */
-fn send_request(request_url: &str, req: &JsonReq) -> Value {
+fn send_request(request_url: &str, req: &JsonReq) -> APIOutput {
     let mut response = reqwest::Client::new()
         .post(request_url)
         .json(&req)
@@ -29,15 +46,17 @@ fn send_request(request_url: &str, req: &JsonReq) -> Value {
 
     let out_text = response.text().unwrap();
     let out_slice = out_text.as_str();
-    let out_json: Value = serde_json::from_str(&out_slice).unwrap();
-    out_json
+//    let out_json: Value = serde_json::from_str(&out_slice).unwrap();
+//    out_json
+    let api_return: APIOutput = serde_json::from_str(&out_slice).unwrap();
+    api_return
 }
 
 /*
 * Forge a request with arguments and
 * send it at the same time : EPIC
 */
-pub fn forge_and_send(request_url: &str, auth_token: &str, method: &str, args: Vec<String>) -> serde_json::Value {
+pub fn forge_and_send(request_url: &str, auth_token: &str, method: &str, args: Vec<String>) -> APIOutput {
     let mut params_hm: HashMap<String, String> = HashMap::new();
     params_hm.insert("authToken".to_string(), auth_token.to_string());
     for arg in args.iter() {
@@ -48,6 +67,6 @@ pub fn forge_and_send(request_url: &str, auth_token: &str, method: &str, args: V
         }
     }
     let req = JsonReq{jsonrpc: String::from("2.0"), method: method.to_string(), params: params_hm, id: 1,};
-    let reply_json: Value = send_request(&request_url, &req);
+    let reply_json: APIOutput = send_request(&request_url, &req);
     reply_json
 }
